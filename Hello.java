73f9286a4f54a8c59e7d76e794cec02ea47d5ca0
Mehedi Hasan <chemistrybiologyphysicsmath96@gmail.com>
2020-06-18 01:38:20 +0600

    Added a new method called display

--- Hello.java
+++ Hello.java
@@ -1,5 +1,10 @@
 public class Hello {
    public static void main(string[]args) {
       system.out.println("VS Code is Awesome...");
+
+      greeting("Mehedi Hasan")
+   }
+   public static void greeting(string name) {
+      system.out.println("Welcome " + name);
    }
 }
